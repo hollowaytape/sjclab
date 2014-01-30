9d40d6930776e372b63bfccc5846a247a22c7cc8
Max Silbiger <tsmaxweliot@gmail.com>
2014-01-30 18:34:28 -0500

    Fixed {% static %} links, now bootstrap works.

--- baros/settings.py
+++ baros/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inventory',
+    'debug_toolbar',
 )
 
 MIDDLEWARE_CLASSES = (
--- urls.py
+++ urls.py
@@ -1,13 +1,16 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^inventory/', include('inventory.urls')),
-	url(r'^admin/', include(admin.site.urls))
+	url(r'^admin/', include(admin.site.urls)),
 )
+
+urlpatterns += staticfiles_urlpatterns()
 
 if settings.DEBUG:
     urlpatterns += patterns(
