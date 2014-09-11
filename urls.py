00fa01304a38fcd707203acedd616d804f238aa9
Max Silbiger <max.silbiger@gmail.com>
2014-09-10 21:37:21 -0400

    Favicon

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.base import RedirectView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,13 +9,14 @@ admin.autodiscover()
 urlpatterns = patterns('',
     url(r'^inventory/', include('inventory.urls')),
 	url(r'^admin/', include(admin.site.urls)),
+    url(r'^favicon\.ico$', RedirectView.as_view(url='https://sjclab-assets.s3.amazonaws.com/img/favicon.ico'), name='favicon'),
 )
 
 urlpatterns += staticfiles_urlpatterns()
 
-if settings.DEBUG:
+"""if settings.DEBUG:
     urlpatterns += patterns(
 	        'django.views.static',
 			(r'media/(?P<path>.*)',
 			'serve',
-			{'document_root': settings.MEDIA_ROOT}), )
+			{'document_root': settings.MEDIA_ROOT}), )"""
