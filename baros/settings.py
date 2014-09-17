95663f71dec69684d0c6a038714043110f7798e2
Max Silbiger <max.silbiger@gmail.com>
2014-09-17 12:29:43 -0400

    Fix dynamic formset bug, fix wrong login redirect path

--- baros/settings.py
+++ baros/settings.py
@@ -110,7 +110,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     "django.core.context_processors.static"
 )
 
-LOGIN_URL = '/registration/login/'
+LOGIN_URL = '/accounts/login/'
 
 #MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
